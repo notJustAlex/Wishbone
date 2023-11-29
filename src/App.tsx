fcddba337699ef8cd590cd04b0575458a63fadc8
Alex <alex.zakablook@gmail.com>
2023-11-29 18:00:00 +0100

    performance optimization 0.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,24 @@
-import AboutSection from "./components/AboutSection/AboutSection";
-import ClientsSection from "./components/ClientsSection/ClientsSection";
-import ContactSection from "./components/ContactSection/ContactSection";
-import FeaturedProjectsSection from "./components/FeaturedProjectsSection/FeaturedProjectsSection";
-import Footer from "./components/Footer/Footer";
+import { lazy } from "react";
+
 import NavMenu from "./components/NavMenu/NavMenu";
-import NewsSectiont from "./components/NewsSection/NewsSection";
-import ProcessSection from "./components/ProcessSection/ProcessSection";
 import ProjectsSection from "./components/ProjectsSection/ProjectsSection";
-import TeamSection from "./components/TeamSection/TeamSection";
+import AboutSection from "./components/AboutSection/AboutSection";
+
+const ClientsSection = lazy(
+	() => import("./components/ClientsSection/ClientsSection")
+);
+const ContactSection = lazy(
+	() => import("./components/ContactSection/ContactSection")
+);
+const FeaturedProjectsSection = lazy(
+	() => import("./components/FeaturedProjectsSection/FeaturedProjectsSection")
+);
+const Footer = lazy(() => import("./components/Footer/Footer"));
+const NewsSectiont = lazy(() => import("./components/NewsSection/NewsSection"));
+const ProcessSection = lazy(
+	() => import("./components/ProcessSection/ProcessSection")
+);
+const TeamSection = lazy(() => import("./components/TeamSection/TeamSection"));
 
 function App() {
 	return (
